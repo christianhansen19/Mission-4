5b16e0728ded77425a88d1a0fcb6bbe8f1bf1b03
Chris Hansen <chris.hansen1298@gmail.com>
2023-01-25 19:36:32 -0700

    Updates

--- Mission 4/wwwroot/js/gradeCalculator.js
+++ Mission 4/wwwroot/js/gradeCalculator.js
@@ -1,30 +1,44 @@
 ﻿$("#calc").click(function () {
-    alert("TEST")
-    // Get form input values
-    var assignments = $("#valueAssignments").val();
-    var groupProjects = $("#valueGroupProjects").val();
-    var quizzes = $("#valueQuizzes").val();
-    var midterm = $("#valueMidterm").val();
-    var finalExam = $("#valueFinal").val();
-    var intex = $("#valueINTEX").val();
+    // Get form input values and assign them to variables
+    var assignments = parseInt($("#valueAssignments").val());
+    var groupProject = parseInt($("#valueGroupProject").val());
+    var quizzes = parseInt($("#valueQuizzes").val());
+    var midterm = parseInt($("#valueMidterm").val());
+    var finalExam = parseInt($("#valueFinal").val());
+    var intex = parseInt($("#valueINTEX").val());
 
-    // Calculate final percentage
-    var finalPercentage = (assignments * 0.5) + (groupProjects * 0.1) + (quizzes * 0.1) + (midterm * 0.1) + (finalExam * 0.1) + (intex * 0.1);
+    // Calculate based on the above variables
+    var finalPercentage = 0.0
+    finalPercentage = (assignments * 0.5) + (groupProject * 0.1) + (quizzes * 0.1) + (midterm * 0.1) + (finalExam * 0.1) + (intex * 0.1);
 
-    // Calculate letter grade
+    // Calculate letter grade based on the final percentage
     var letterGrade;
     if (finalPercentage >= 94) {
         letterGrade = "A";
     } else if (finalPercentage >= 90) {
-        letterGrade = "B";
+        letterGrade = "A-";
     } else if (finalPercentage >= 87) {
-        letterGrade = "C";
+        letterGrade = "B+";
     } else if (finalPercentage >= 84) {
+        letterGrade = "B";
+    } else if (finalPercentage >= 80) {
+        letterGrade = "B-";
+    } else if (finalPercentage >= 77) {
+        letterGrade = "C+";
+    } else if (finalPercentage >= 74) {
+        letterGrade = "C";
+    } else if (finalPercentage >= 80) {
+        letterGrade = "C-";
+    } else if (finalPercentage >= 67) {
+        letterGrade = "D+";
+    } else if (finalPercentage >= 64) {
         letterGrade = "D";
+    } else if (finalPercentage >= 60) {
+        letterGrade = "D-";
     } else {
-        letterGrade = "F";
+        letterGrade = "E";
     }
 
-    // Display result
-    $("#result").innerHTML = "Final Percentage: " + finalPercentage + "%" + '\n' + letterGrade
+    // Display result in the form tag
+    $("#result").html("Final Percentage: " + finalPercentage.toFixed(2).toString() + "%" + '\n' + letterGrade)
 })
